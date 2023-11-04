08236032220e20af46a6a4c6c93d93814439157e
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2023-11-04 13:41:02 +0100

    Added sections on page

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import {Header} from "../Header/Header";
 import classes from "./Page.module.css";
+import {Dictionary} from "../../utils/Dictionary";
 
 export function Page() {
+    const sections: Dictionary<string, string>[] = [
+        {key: "Home", value: "#"},
+        {key: "About Me", value: "#aboutme"},
+        {key: "Contact", value: "#contact"}
+    ];
+
     return (
         <div className={classes.mainContainer}>
-            <Header/>
+            <Header sections={sections}/>
         </div>
     );
 }
