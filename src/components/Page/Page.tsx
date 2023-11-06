15aa07b1c8b2544184f89a7113e899a62bbd24a1
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2023-11-06 23:08:10 +0100

    Added WelcomeScreen and new sections to the NavBar, updated styles

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, {useRef} from "react";
 import {Header} from "../Header/Header";
-import classes from "./Page.module.css";
+import classes from "./Page.module.scss";
 import {Dictionary} from "../../utils/Dictionary";
+import {useInView} from "framer-motion";
+import {WelcomeScreen} from "../WelcomeScreen/WelcomeScreen";
+
 
 export function Page() {
     const sections: Dictionary<string, string>[] = [
         {key: "Home", value: "#"},
-        {key: "About Me", value: "#aboutme"},
+        {key: "About", value: "#aboutme"},
+        {key: "Experience", value: "#experience"},
         {key: "Contact", value: "#contact"}
     ];
 
+    const welcomeScreenRef = useRef(null);
+    const welcomeScreenIsInView = useInView(welcomeScreenRef);
+
     return (
         <div className={classes.mainContainer}>
-            <Header sections={sections}/>
+            <Header
+                sections={sections}
+                welcomePhotoIsInView={welcomeScreenIsInView}
+            />
+
+            <WelcomeScreen welcomeScreenRef={welcomeScreenRef}/>
+
+            <div className={classes.contentContainer}>
+            </div>
         </div>
     );
 }
