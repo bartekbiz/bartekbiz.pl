0d46724cf2bd693dcdaedea44e4522bec4685467
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2023-11-06 23:05:12 +0100

    Added header animation and changed it's looks

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,45 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import {NavBar} from "../NavBar/NavBar";
 import {Dictionary} from "../../utils/Dictionary";
 import classes from "./Header.module.scss";
+import {motion, useAnimation} from "framer-motion";
 
 interface HeaderProps {
-    sections: Dictionary<string, string>[]
+    sections: Dictionary<string, string>[],
+    welcomePhotoIsInView: boolean
 }
-export function Header({sections}: HeaderProps) {
+export function Header({sections, welcomePhotoIsInView}: HeaderProps) {
+    const mainControls = useAnimation();
+
+    useEffect(() => {
+        if (!welcomePhotoIsInView) {
+            mainControls.start("visible");
+        }
+        else {
+            mainControls.start("hidden");
+        }
+
+    }, [welcomePhotoIsInView]);
+
     return (
-        <div className={classes.headerContainer}
-        >
-            <div className={classes.logoContainer}>
-                <h2>Bartosz Bizoń</h2>
+        <div>
+            <motion.div
+                className={classes.headerBackground}
+                variants={{
+                    hidden: {opacity: 0, y: -10},
+                    visible: {opacity: 1, y: 0}
+                }}
+                initial={"hidden"}
+                animate={mainControls}
+                transition={{duration: 0.4}}
+            />
+
+            <div className={classes.headerContentsContainer}>
+                <div className={classes.logoContainer}>
+                    <h2 style={{whiteSpace: "nowrap"}}>Bartosz Bizoń</h2>
+                </div>
+                <NavBar sections={sections}/>
             </div>
-            <NavBar sections={sections}/>
         </div>
     );
 }
