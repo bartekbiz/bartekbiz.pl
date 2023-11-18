c1f60b00ce0f2fe9456469099d870133567abad5
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2023-11-18 11:40:16 +0100

    Created new section 'About'

---
+++ src/components/SectionAbout/SectionAbout.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import sectionClasses from "../../sharedModules/Section.module.scss";
+
+export function SectionAbout() {
+    return (
+        <div id={"aboutme"} style={{marginTop: "0.2rem"}}>
+            <div className={sectionClasses.sectionContainer}>
+                <div className={sectionClasses.sectionTitle}>
+                    About
+                </div>
+                <div className={sectionClasses.sectionText}>
+                    Sit quaerat asperiores ipsa non omnis et.
+                    Ratione ipsum nisi eius aliquid molestiae rerum est autem.
+                    Odio quibusdam qui iste aut quia rem in.
+                    Debitis animi voluptas voluptatibus.
+                    Praesentium qui unde quas voluptatem.
+                    Ullam ipsum quia maxime quasi quam.
+                    Sit quaerat asperiores ipsa non omnis et.
+                    Ratione ipsum nisi eius aliquid molestiae rerum est autem.
+                    Odio quibusdam qui iste aut quia rem in.
+                    Debitis animi voluptas voluptatibus.
+                    Praesentium qui unde quas voluptatem.
+                    Ullam ipsum quia maxime quasi quam.
+                </div>
+            </div>
+        </div>
+    );
+}
