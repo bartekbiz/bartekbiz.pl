a5a6e76b603b639987940d595df526553d57c232
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2023-12-19 18:48:38 +0100

    Creted 3 sections with Section component

---
+++ src/components/Section/SectionContact/SectionContact.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import sectionClasses from "../../../sharedModules/Section.module.scss";
+import {Section} from "../Section";
+
+export function SectionContact() {
+    const sectionName: string = "Contact";
+
+    return (
+        <Section id={sectionName.toLowerCase()} title={sectionName}>
+            <div className={sectionClasses.sectionText}>
+                Sit quaerat asperiores ipsa non omnis et.
+                Ratione ipsum nisi eius aliquid molestiae rerum est autem.
+                Odio quibusdam qui iste aut quia rem in.
+                Debitis animi voluptas voluptatibus.
+                Praesentium qui unde quas voluptatem.
+                Ullam ipsum quia maxime quasi quam.
+                Sit quaerat asperiores ipsa non omnis et.
+                Ratione ipsum nisi eius aliquid molestiae rerum est autem.
+                Odio quibusdam qui iste aut quia rem in.
+                Debitis animi voluptas voluptatibus.
+                Praesentium qui unde quas voluptatem.
+                Ullam ipsum quia maxime quasi quam.
+            </div>
+        </Section>
+    );
+}
