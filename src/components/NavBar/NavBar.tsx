012d9b81f3ee03364a7cdb01c866ed266f7e0376
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2023-12-19 18:49:42 +0100

    Tiny refactoring

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,9 +3,11 @@ import {NavBarLink} from "./NavBarLink";
 import {Dictionary} from "../../utils/Dictionary";
 import classes from "./NavBar.module.scss";
 
+
 interface NavBarProps {
     sections: Dictionary<string, string>[]
 }
+
 export function NavBar({sections}: NavBarProps) {
     return (
         <ul className={classes.navbarUl}>
